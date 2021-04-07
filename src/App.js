293b7f302cc1485f0b13bc8916134f84f2469b33
omonmunduz <omurbekn@gmail.com>
2021-04-07 11:04:42 -0700

    reveal animation added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Fade from 'react-reveal/Fade';
+import Bounce from 'react-reveal/Bounce';
+
 import "./App.css";
 import Hero from "./components/Hero";
 import Feature from "./components/Feature";
@@ -10,18 +13,33 @@ import EsimMore from "./components/EsimMore";
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 
+
+
+
 function App() {
   return (
     <div className="App ">
       <Hero />
-      <Feature />
-      <Stats />
-      <Products />
-      <PortableWifi />
-      <PortableMore />
-      <Esim />
-      <EsimMore />
+      <Fade>
+        <Feature />
+      </Fade>
+      <Fade>
+        <Stats />
+      </Fade>
+      <Bounce bottom>
+        <Products />
+      </Bounce>
+      <Bounce left> 
+        <PortableWifi />
+        <PortableMore/>
+      </Bounce>
+      <Bounce right>
+        <Esim />
+        <EsimMore />
+      </Bounce>
+      <Bounce bottom>
       <ContactUs />
+      </Bounce>
       <Footer />
     </div>
   );
